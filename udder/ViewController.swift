a3d597ee04368a76a7badf42568707601015935e
GMHCode <greghughes988@gmail.com>
2018-11-19 16:49:47 -0700

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Greg Hughes on 11/19/18.
+//  Copyright © 2018 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
